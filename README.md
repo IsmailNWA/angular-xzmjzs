32c0e9aae41e7a73d9bbd0d5e594f2ae216ee51c
IsmailNWA <ismail.dinar.nwa@gmail.com>
2022-05-13 12:39:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-xzmjzs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-xzmjzs)
---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { WindowService, WindowRef, WindowCloseResult } from '@progress/kendo-angular-dialog';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <div class="example-wrapper">
+            <button kendoButton (click)="openWindow()">Open Window</button>
+        </div>
+
+        <div kendoWindowContainer></div>
+    `
+})
+export class AppComponent {
+    constructor(private windowService: WindowService) {}
+
+    public openWindow(): void {
+        const window: WindowRef = this.windowService.open({
+            title: 'My Window',
+            content: 'My Content!',
+            width: 300,
+            height: 200
+        });
+
+        window.result.subscribe((result) => {
+            if (result instanceof WindowCloseResult) {
+                console.log('Window was closed!');
+            }
+        });
+
+        window.window.instance.widthChange.subscribe(() => {
+            if(window.window.instance.width > 500) {
+                window.window.instance.setDimension('width', 500);
+            }
+        })
+    }
+}
